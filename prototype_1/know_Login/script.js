f8758bf32ec5b016b53fd820656d2944d2ee68a2
pm4c1 <122862249+pm4c1@users.noreply.github.com>
2025-03-13 16:54:49 +0900

    Create script.js

---
+++ prototype_1/know_Login/script.js
@@ -0,0 +1,50 @@
+
+// login
+let switchCtn = document.querySelector("#switch-cnt");
+let switchC1 = document.querySelector("#switch-c1");
+let switchC2 = document.querySelector("#switch-c2");
+let switchCircle = document.querySelectorAll(".switch__circle");
+let switchBtn = document.querySelectorAll(".switch-btn");
+let aContainer = document.querySelector("#a-container");
+let bContainer = document.querySelector("#b-container");
+let allButtons = document.querySelectorAll(".submit");
+
+let getButtons = (e) => e.preventDefault()
+
+let changeForm = (e) => {
+
+    switchCtn.classList.add("is-gx");
+    setTimeout(function () {
+        switchCtn.classList.remove("is-gx");
+    }, 1500)
+
+    switchCtn.classList.toggle("is-txr");
+    switchCircle[0].classList.toggle("is-txr");
+    switchCircle[1].classList.toggle("is-txr");
+
+    switchC1.classList.toggle("is-hidden");
+    switchC2.classList.toggle("is-hidden");
+    aContainer.classList.toggle("is-txl");
+    bContainer.classList.toggle("is-txl");
+    bContainer.classList.toggle("is-z200");
+}
+
+let mainF = (e) => {
+    for (var i = 0; i < allButtons.length; i++)
+        allButtons[i].addEventListener("click", getButtons);
+    for (var i = 0; i < switchBtn.length; i++)
+        switchBtn[i].addEventListener("click", changeForm)
+}
+
+window.addEventListener("load", mainF);
+
+// loading
+document.addEventListener("DOMContentLoaded", function () {
+    // 로딩 애니메이션 숨기기
+    setTimeout(() => {
+        document.querySelector(".loading").style.display = "none";
+    }, 1500); // 1.5초 후 로딩 제거
+
+    // 페이지 페이드 인 효과
+    document.querySelector(".content").style.opacity = "1";
+});
